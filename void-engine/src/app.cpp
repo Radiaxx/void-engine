e1ecd6dc8281efd13e5a08e40058154555a2d785
Radiax <danielescrima@outlook.it>
2022-11-22 12:29:32 +0100

    Prototyped game objects

--- void-engine/src/app.cpp
+++ void-engine/src/app.cpp
@@ -4,21 +4,23 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm.hpp>
+#include <gtc/constants.hpp>
 
 // std
 #include <stdexcept>
-#include <cassert>
+#include <cassert>	
 #include <array>
 
 namespace ve {
 
 	struct SimplePushConstantData {
+		glm::mat2 transform{1.f};
 		glm::vec2 offset;
 		alignas(16) glm::vec3 color;
 	};
 
 	App::App() {
-		loadModels();
+		loadGameObjects();
 		createPipelineLayout();
 		recreateSwapChain();
 		createCommandBuffers();
@@ -38,14 +40,23 @@ namespace ve {
 		vkDeviceWaitIdle(veDevice.device());	
 	}
 
-	void App::loadModels() {
+	void App::loadGameObjects() {
 		std::vector<VeModel::Vertex> vertices{
 			{{0.0f, -0.5f}, {1.0f, 0, 0}},
 			{{0.5f, 0.5f}, {0, 1.0f, 0}},
 			{{-0.5f, 0.5f}, {0, 0, 1.0f}}
 		};
 
-		veModel = std::make_unique<VeModel>(veDevice, vertices);
+		auto veModel = std::make_shared<VeModel>(veDevice, vertices);
+
+		auto triangle = VeGameObject::createGameObject();
+		triangle.model = veModel;
+		triangle.color = { .1f, .8f, .1f };
+		triangle.transform2d.translation.x = .2f;	
+		triangle.transform2d.scale = { 2.f, .5f };
+		triangle.transform2d.rotation = .25f * glm::two_pi<float>();
+
+		gameObjects.push_back(std::move(triangle)); 
 	}
 
 	void App::createPipelineLayout() {
@@ -125,9 +136,6 @@ namespace ve {
 	}
 
 	void App::recordCommandBuffer(int imageIndex) {
-		static int frame = 0;
-		frame = (frame + 1) % 1000;
-
 		VkCommandBufferBeginInfo beginInfo{};
 		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -162,30 +170,37 @@ namespace ve {
 		vkCmdSetViewport(commandBuffers[imageIndex], 0, 1, &viewport);
 		vkCmdSetScissor(commandBuffers[imageIndex], 0, 1, &scissor);
 
-		vePipeline->bind(commandBuffers[imageIndex]);
-		veModel->bind(commandBuffers[imageIndex]);
-
-		for (int i = 0; i < 4; i++) {
-			SimplePushConstantData push{};
-			push.offset = { -0.5f + frame * 0.02f, -0.4f + i * 0.25f };
-			push.color = { 0.0f, 0.0f, 0.2f + 0.2f * i};
-
-			vkCmdPushConstants(
-				commandBuffers[imageIndex],
-				pipelineLayout,
-				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
-				0, 
-				sizeof(SimplePushConstantData),
-				&push
-			);
-
-			veModel->draw(commandBuffers[imageIndex]);
-		}
+		renderGameObjects(commandBuffers[imageIndex]);
 
 		vkCmdEndRenderPass(commandBuffers[imageIndex]);
 
 		if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS) {
 			throw std::runtime_error("failed to record command buffer");
+		}
+	}
+
+	void App::renderGameObjects(VkCommandBuffer commandBuffer) {
+		vePipeline->bind(commandBuffer);
+
+		for (auto& obj : gameObjects) {
+			obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.01f, glm::two_pi<float>());
+
+			SimplePushConstantData push{};
+			push.offset = obj.transform2d.translation;
+			push.color = obj.color;
+			push.transform = obj.transform2d.mat2();
+
+			vkCmdPushConstants(
+				commandBuffer,
+				pipelineLayout,
+				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+				0,
+				sizeof(SimplePushConstantData),
+				&push
+			);
+
+			obj.model->bind(commandBuffer);
+			obj.model->draw(commandBuffer);
 		}
 	}
 
--- void-engine/src/app.hpp
+++ void-engine/src/app.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "ve_window.hpp"
-#include "ve_pipeline.hpp"
 #include "ve_device.hpp"
+#include "ve_pipeline.hpp"
 #include "ve_swap_chain.hpp"
-#include "ve_model.hpp"
+#include "ve_game_object.hpp"
 
 // std
 #include <memory>
@@ -25,7 +25,7 @@ namespace ve {
 		void run();	 
 
 	private:
-		void loadModels();
+		void loadGameObjects();
 		void createPipelineLayout();
 		void createPipeline();
 		void createCommandBuffers();
@@ -33,6 +33,7 @@ namespace ve {
 		void drawFrame();
 		void recreateSwapChain();
 		void recordCommandBuffer(int imageIndex);
+		void renderGameObjects(VkCommandBuffer commandBuffer);
 
 		VeWindow veWindow{WIDTH, HEIGTH, "Hello Vulkan!"};
 		VeDevice veDevice{ veWindow };
@@ -40,7 +41,7 @@ namespace ve {
 		std::unique_ptr<VePipeline> vePipeline;
 		VkPipelineLayout pipelineLayout;
 		std::vector<VkCommandBuffer> commandBuffers;	
-		std::unique_ptr<VeModel> veModel;
+		std::vector<VeGameObject> gameObjects;
 
 		std::string vertFilepath = "C:/Users/danie/OneDrive/Documenti/Visual Studio 2022/Projects/void-engine/void-engine/src/shaders/simple_shader.vert.spv";
 		std::string fragFilepath = "C:/Users/danie/OneDrive/Documenti/Visual Studio 2022/Projects/void-engine/void-engine/src/shaders/simple_shader.frag.spv";

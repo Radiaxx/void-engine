287f24f91fabe676a715449618e558bbcabbbb8a
Radiax <danielescrima@outlook.it>
2022-11-28 23:28:42 +0100

    Add Angles & Coordinates

--- void-engine/src/ve_game_object.hpp
+++ void-engine/src/ve_game_object.hpp
@@ -2,22 +2,27 @@
 
 #include "ve_model.hpp"
 
+// libs
+#include <gtc/matrix_transform.hpp>
+
 // std
 #include <memory>
 
 namespace ve {
-	struct Transform2dComponent {
-		glm::vec2 translation{};	// Position offset
-		glm::vec2 scale{ 1.f, 1.f };
-		float rotation;
+	struct TransformComponent {
+		glm::vec3 translation{};	// Position offset
+		glm::vec3 scale{ 1.f, 1.f, 1.f };
+		glm::vec3 rotation{};
 
-		glm::mat2 mat2() { 
-			const float s = glm::sin(rotation);
-			const float c = glm::cos(rotation);
-			glm::mat2  rotMatrix{ {c, s}, {-s, c} };
+		glm::mat4 mat4() {
+			auto transform = glm::translate(glm::mat4{1.f}, translation);
 
-			glm::mat2 scaleMat{ {scale.x, .0f}, {.0f, scale.y} };
-			return rotMatrix * scaleMat;
+			transform = glm::rotate(transform, rotation.y, { 0.f, 1.f, 0.f });
+			transform = glm::rotate(transform, rotation.x, { 1.f, 0.f, 0.f });
+			transform = glm::rotate(transform, rotation.x, { 0.f, 0.f, 1.f });	
+
+			transform = glm::scale(transform, scale);
+			return transform;
 		}
 	};
 
@@ -39,7 +44,7 @@ namespace ve {
 
 		std::shared_ptr<VeModel> model{};
 		glm::vec3 color{};
-		Transform2dComponent transform2d{};
+		TransformComponent transform{};
 
 	private:
 		VeGameObject(id_t objId) : id{objId} {}
